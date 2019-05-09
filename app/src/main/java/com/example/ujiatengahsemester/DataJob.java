15d958a37a7f73e16e541c3d8360c80e6d3c170c
Jack <dzaki.frachman@gmail.com>
2019-05-09 07:33:00 +0700

    first commit

---
+++ app/src/main/java/com/example/ujiatengahsemester/DataJob.java
@@ -0,0 +1,54 @@
+package com.example.ujiatengahsemester;
+
+class DataJob {
+
+    String posisi;
+    String perusahaan;
+    String lokasi;
+    String imageurl;
+    String deskripsi;
+    String about;
+    String senior;
+    String type;
+    String func;
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getPerusahaan() {
+        return perusahaan;
+    }
+
+    public void setPerusahaan(String perusahaan) {
+        this.perusahaan = perusahaan;
+    }
+
+    public String getPosisi() {
+        return posisi;
+    }
+
+    public void setPosisi(String posisi) {
+        this.posisi = posisi;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+}
